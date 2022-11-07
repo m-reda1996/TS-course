e49ddbf7159f137b9ef35ebdfe7063931c5cf438
Mahmoud Reda <m.elshentenawy1996@gmail.com>
2022-11-07 11:47:49 +0200

    enums

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 "use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/posts/1';
-axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
-});
+function compare(num1, num2) {
+    if (num1 === num2) {
+        return 0;
+    }
+    else if (num1 > num2) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+console.log(compare(23, 32));
+console.log(compare(33, 32));
+console.log(compare(23, 23));
+let article = [12, "sadf", true];
+const [id, title, published] = article;
+function logging(msg) {
+    console.log(msg);
+    return;
+}
+console.log(logging('asdfasdfsdf'));
+var levels;
+(function (levels) {
+    levels[levels["kids"] = 9] = "kids";
+    levels[levels["easy"] = 12] = "easy";
+    levels[levels["mudium"] = 15] = "mudium";
+    levels[levels["hard"] = 20] = "hard";
+})(levels || (levels = {}));
+let lvl = "Easy";
+if (lvl === "Easy") {
+    console.log(`the level is ${lvl} and the number of sec is ${levels.easy}`);
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,45 @@
-import axios from "axios"
+type nums = 0 | 1| -1
+
+function compare(num1 : number , num2 : number ) : nums {
+    if (num1 === num2){
+      return 0
+    } else if (num1 > num2) {
+      return 1
+    } else {
+      return -1
+    }
+}
+
+
+console.log(compare(23,32))
+console.log(compare(33,32))
+console.log(compare(23,23))
+
+let article : [number, string , boolean ] = [ 12, "sadf" , true] 
+
+ const [id , title , published ] = article
+
+
+ function logging (msg : string) :void {
+  console.log(msg)
+  return ;
+ }
+
+ console.log(logging('asdfasdfsdf'))
+
+
+enum levels {
+  kids = 9 , 
+  easy = 12 , 
+  mudium = 15, 
+  hard = 20
+}
+
+let lvl :string = "Easy"
+
+
+if (lvl === "Easy") {
+  console.log(`the level is ${lvl} and the number of sec is ${levels.easy}`)
+}
+
 
